453837851836d0c710e643407b20a0f654ca21f1
Stephen Young <stephen.young.dev@gmail.com>
2020-02-27 23:31:10 -0600

    tweaking the crow parameters

--- Actors/Crow/Crow.gd
+++ Actors/Crow/Crow.gd
@@ -5,7 +5,7 @@ var _highlight = 0.0
 func _process(_delta):
 	look_at(global_position + _velocity)
 	
-	if _flock_size and _highlight < _flock_size / 20.0:
+	if _flock_size and _highlight < _flock_size / 10.0:
 		_highlight += _flock_size / 1000.0
 	elif _highlight > 0:
 		_highlight -= 0.01
